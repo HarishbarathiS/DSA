88338aa6bb0f95907fcc3b5d9a14fbb8c9df6e5a
Harish b.S <96436224+HarishbarathiS@users.noreply.github.com>
2023-05-29 14:00:00 +0530

    Update maxsubarraysum.cpp
    added problem statement

--- Array/maxsubarraysum.cpp
+++ Array/maxsubarraysum.cpp
@@ -1,3 +1,9 @@
+/*
+Problem Statement : Given an integer array arr, find the contiguous subarray (containing at least one number) which
+has the largest sum and returns its sum and print the subarray.
+*/
+
+
 #include <bits/stdc++.h>
 #include <climits>
 using namespace std;
